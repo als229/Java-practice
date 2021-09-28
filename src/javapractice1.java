eeb51a3630af11e25eff0bae82a8bd2fffe7b9ce
김관민 <@DESKTOP-A5HFT58>
2021-09-28 21:33:54 +0900

    3-4

--- src/javapractice1.java
+++ src/javapractice1.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 
 public class javapractice1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		
+		int box = 10;
+		int num = 0;
+		
+		num = a%box == 0 ? a/box : a/box +1; 
+		
+		System.out.println(num);
 	}
 
 }
