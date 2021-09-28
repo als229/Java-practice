8f3601135cd89b1109e4ba328a378cd83c4ed492
김관민 <@DESKTOP-A5HFT58>
2021-09-28 21:51:17 +0900

    3-6

--- src/javapractice/javapractice1.java
+++ src/javapractice/javapractice1.java
@@ -1,3 +1,5 @@
+package javapractice;
+
 import java.util.Scanner;
 
 public class javapractice1 {
---
+++ src/javapractice/practice36.java
@@ -0,0 +1,13 @@
+package javapractice;
+
+public class practice36 {
+
+	public static void main(String[] args) {
+		int hwa = 100;
+		float sup = (int)((5/9f * (hwa-32))*100+0.5f)/100f;
+		
+		System.out.println("hwa :"+ hwa);
+		System.out.println("sup :"+ sup);
+	}
+
+}
