d4b9370474746647454ee5cc1bdbf978baa6d833
als229 <rhksalsdldia@naver.com>
2021-10-07 22:47:46 +0900

    5-4

--- src/javapractice/arraypractice.java
+++ src/javapractice/arraypractice.java
@@ -1,14 +1,40 @@
 package javapractice;
 
+
+//5-3 Á¤´ä
+//public class arraypractice {
+//
+//	public static void main(String[] args) {
+//		int [] arr= {10, 20, 30, 40, 50};
+//		int sum = 0;
+//		
+//		for(int i = 0; i <arr.length;i++) {
+//			sum += arr[i]; 
+//		}System.out.println("sum = "+ sum);
+//	}
+//
+//}
+
 public class arraypractice {
 
 	public static void main(String[] args) {
-		int [] arr= {10, 20, 30, 40, 50};
-		int sum = 0;
+		int [][] arr= {
+				{5,5,5,5,5},
+				{10,10,10,10,10},
+				{20,20,20,20,20},
+				{30,30,30,30,30}
+		};
 		
-		for(int i = 0; i <arr.length;i++) {
-			sum += arr[i]; 
-		}System.out.println("sum = "+ sum);
+		int total=0;
+		float average =0;
+		
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j<arr[i].length;j++) {
+				total +=arr[i][j];
+			}
+			average = total/20f;
+		}System.out.println("total = "+ total);
+		System.out.println("average = "+ average);
 	}
 
 }
