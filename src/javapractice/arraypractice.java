2bbe3417ec9cae8ddc060e854b87d10f6e7c82ef
als229 <rhksalsdldia@naver.com>
2021-10-09 00:10:37 +0900

    5-6 배열 끝

--- src/javapractice/arraypractice.java
+++ src/javapractice/arraypractice.java
@@ -1,5 +1,6 @@
 package javapractice;
 
+import java.util.Scanner;
 
 //5-3 정답
 //public class arraypractice {
@@ -70,10 +71,36 @@ package javapractice;
 //		
 //	}
 //}
+
+
+//5-6 어렵다 다시한번 봐 
 public class arraypractice {
 
 	public static void main(String[] args) {
+		String[] words = {"television", "computer", "mouse", "phone"};
 		
+		Scanner scanner = new Scanner(System.in);
+		
+		for(int i = 0; i < words.length; i++) {
+			char[] question = words[i].toCharArray();
+			
+			for(int j =0; j<question.length;j++) {
+				char tmp = ' ';
+				int c = (int)(Math.random()*question.length);
+				
+				tmp = question[j];
+				question[j] = question[c];
+				question[c] = tmp;
+			}
+			System.out.printf("Q%d. %s 의 정답을 입력하세요",i+1, new String(question));
+			
+			String answer = scanner.nextLine();
+			
+			if(words[i].equals(answer.trim())) {
+				System.out.println("정답이다 임마");
+			}else 
+				System.out.printf("틀렸다 멍청아 ~%n");
+		}
 	}
 }
 
