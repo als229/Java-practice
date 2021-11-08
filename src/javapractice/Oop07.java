ef2859221c1cb30d219cad6374d615bc8798522a
als229 <rhksalsdldia@naver.com>
2021-11-08 21:01:34 +0900

    자바의 정석 7장 연습문제를 풀자

---
+++ src/javapractice/Oop07.java
@@ -0,0 +1,41 @@
+package javapractice;
+
+class SutdaDeck {
+	
+	final int CARD_NUM = 20;
+	SutdaCard[] cards = new SutdaCard[CARD_NUM];
+	
+	SutdaDeck() {
+		for(int i = 0; i < cards.length; i++) {
+			cards[i] = ;
+		}
+	}
+}
+
+class SutdaCard{
+	int num;
+	boolean isKwang;
+	
+	SutdaCard () {
+		this(1, true);
+	}
+	
+	SutdaCard(int num, boolean isKwang){
+		this.num = num;
+		this.isKwang = isKwang;
+	}
+	
+	public String toString() {
+		return num + (isKwang ? "K" : "");
+	}
+}
+
+class Oop07{
+	public static void main(String[] args) {
+		SutdaDeck deck = new SutdaDeck();
+		
+		for (int i = 0; i < deck.cards.length; i++) {
+			System.out.println(deck.cards[i] + " , ");
+		}
+	}
+}
